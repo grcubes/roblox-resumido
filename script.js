4fc43eff796a89d0681db3ecce26c557cb791f70
grcubes <gustavosamrod@gmail.com>
2025-08-19 10:47:49 -0700

    dark-mode-patch

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ let useDarkMode = false;
 const darkModeButton = document.getElementById("dark-mode-button");
 
 darkModeButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+    useDarkMode = !useDarkMode;
+
+    if (useDarkMode)
+    {
+        document.body.classList.add("dark-mode");
+        darkModeButton.textContent = "Light Mode";
+    }
+    else
+    {
+        document.body.classList.remove("dark-mode");
+        darkModeButton.textContent = "Dark Mode";
+    }
 })
